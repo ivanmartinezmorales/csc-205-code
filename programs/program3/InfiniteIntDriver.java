72b9c155910e2194f8d6c6c6ad66fc5477655543
Ivan Martinez Morales <ivan.martinez.morales@gmail.com>
2019-04-23 11:19:05 -0700

    add testCompareTo() for testing compareTo, add, equals

--- programs/program3/InfiniteIntDriver.java
+++ programs/program3/InfiniteIntDriver.java
@@ -3,15 +3,29 @@ import java.util.*;
 public class InfiniteIntDriver {
 
     private static String[] testCases = {"100,000,000,000", "123,123,123,123", "123", "485,123,127,055", "1", "192,123,123,123,123,123"};
-    
+
     public static void main(String[] args) {
+
+        /**
+         * ROUND ONE OF TESTING! Testing toString, and divisibility functions.
+         */
        print("setting up testing environment");
         for (String s : testCases) {
             testCase(s);        
         }
-        print("testing complete!");
+        print("First batch of testing complete!");
+
+        /**
+         * ROUND TWO OF TEST CASES: TESTING compareTo, add, and equals
+         */
+        print("Setting up testing environment for second round of tests");
+        for (int i = 1; i < testCases.length; i++) {
+            testCompareTo(testCases[i], testCases[i-1]);
+        }
+
+        print("Second round of test cases complete.");
     }
-    
+
     public static void testCase(String infiniteString) {
 
         // 1. Create our infinite int.
@@ -33,6 +47,17 @@ public class InfiniteIntDriver {
 
         print("End of testing. Methods not tested: compareTo(), equals(), and add()");
         print("\n\n\n");
+    }
+
+    public static void testCompareTo(String infinteString, String infiniteStringTwo) {
+        System.out.printf("Creating number with: %s\n", infiniteString);
+        InfiniteInt number = new InfiniteInt(infiniteString);
+        System.out.printf("Creating second number with: %s\n", infiniteStringTwo);
+        InfiniteInt number2 = new InfiniteInt(infiniteStringTwo);
+
+        print("Testing compareTo method");
+        System.out.printf("The return of the comparison is: %d", number.compareTo(number2));
+        print("Finished comparing two numbers.");
     }
 
     public static void print(String printString) {
